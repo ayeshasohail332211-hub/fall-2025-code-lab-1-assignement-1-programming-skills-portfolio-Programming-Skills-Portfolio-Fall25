ed97ad9280ca6996fda53d8bf916904a2797b9f3
ayeshasohail332211-hub <ayeshasohail332211@gmail.com>
2025-10-16 23:25:51 +0400

    Create Exercise09_Ayesha Sohail.py
    Completed the given blanks in the code.

---
+++ Exercise 09 -Hello/Exercise09_Ayesha Sohail.py
@@ -0,0 +1,9 @@
+#Completing the blanks
+def hello():
+    print ("Hello") # Fill in this blank to print "Hello" to the console
+
+def main():
+    hello() # Fill in this blank to call the hello() function
+
+if __name__ == "__main__":
+    main()
