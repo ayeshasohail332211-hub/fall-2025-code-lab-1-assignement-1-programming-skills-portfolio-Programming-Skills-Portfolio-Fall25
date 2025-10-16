dde14b2ab8d158638d9af04caa9ac24ff6f84f9e
ayeshasohail332211-hub <ayeshasohail332211@gmail.com>
2025-10-16 23:47:01 +0400

    Create Exercise04- Ayesha Sohail.py
    Created a program that asks the capital of France and provides feedback based on the user's answer.

---
+++ Exercise 04 -Primitive Quiz/Exercise04- Ayesha Sohail.py
@@ -0,0 +1,8 @@
+#Ask the user the question
+capital= input ("What is the capital of France? ")
+
+#Check is the answer is correct
+if capital.lower() == "paris":
+    print("The answer is correct!")
+else:
+    print("The answer is wrong! It is Paris.") 
