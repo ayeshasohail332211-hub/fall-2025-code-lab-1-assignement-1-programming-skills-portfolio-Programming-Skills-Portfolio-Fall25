2a45a6fea9269863cfb42681d45c6b9bf4a2715d
ayeshasohail332211-hub <ayeshasohail332211@gmail.com>
2025-11-19 23:12:25 +0400

    Update Exercise04- Ayesha Sohail.py
    Updated code with changes

--- Exercise 04 -Primitive Quiz/Exercise04- Ayesha Sohail.py
+++ Exercise 04 -Primitive Quiz/Exercise04- Ayesha Sohail.py
@@ -1,8 +1,8 @@
 #Ask the user the question
 capital= input ("What is the capital of France? ")
 
-#Check is the answer is correct
-if capital.lower() == "paris":
+#Check if the answer is correct
+if capital== "paris":
     print("The answer is correct!")
 else:
     print("The answer is wrong! It is Paris.") 
